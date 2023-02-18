395d73a6999ff34b1581f3f9708358d56ae60ec5
thandeka <thandekaportiap07@gmail.com>
2023-02-18 04:04:41 -0800

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,4 @@
-#include <stdio>
+#include <stdio.h>
 
 /**
  * main - Prints the alphabet.
@@ -11,7 +11,7 @@ int main(void)
 	int i;
 
 	for
-		(i = 0; i, 26 i++);
+		((i = 0); i; 26); i++;);
 	{
 		putchar(alp[i]);
 	}
