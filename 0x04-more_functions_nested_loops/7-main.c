d9581ca4c871083f7a179a8ed6da42629cbef2ce
thandeka <thandekaportiap07@gmail.com>
2023-02-24 08:01:55 -0800

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_disgonal(0);
+    print_disgonal(2);
+    print_disgonal(10);
+    print_disgonal(-4);
+    return (0);
+}
