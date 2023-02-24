280c039a016d944104c0f52d43bcff85fd09ded6
thandeka <thandekaportiap07@gmail.com>
2023-02-24 08:20:35 -0800

    diagonal

--- 0x04-more_functions_nested_loops/7-main.c
+++ 0x04-more_functions_nested_loops/7-main.c
@@ -7,9 +7,9 @@
  */
 int main(void)
 {
-    print_disgonal(0);
-    print_disgonal(2);
-    print_disgonal(10);
-    print_disgonal(-4);
+    print_diagonal(0);
+    print_diagonal(2);
+    print_diagonal(10);
+    print_diagonal(-4);
     return (0);
 }
